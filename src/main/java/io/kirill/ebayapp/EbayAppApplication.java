5a6aa5a8c4decec7f4618a11777fed8da3a38962
kirill5k <immotional@aol.com>
2019-10-26 09:51:09 +0100

    Initial commit

---
+++ src/main/java/io/kirill/ebayapp/EbayAppApplication.java
@@ -0,0 +1,13 @@
+package io.kirill.ebayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbayAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/kirill/ebayapp/EbayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kirill.ebayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
