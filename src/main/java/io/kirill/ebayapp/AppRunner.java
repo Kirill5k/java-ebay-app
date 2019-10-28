4d2dd0a56bd83dbecf6ba200d04dacfe91d75a7d
kirill <immotional@aol.com>
2019-10-28 10:24:06 +0000

    changed app runner implementation

--- src/main/java/io/kirill/ebayapp/AppRunner.java
+++ src/main/java/io/kirill/ebayapp/AppRunner.java
@@ -2,32 +2,28 @@ package io.kirill.ebayapp;
 
 import io.kirill.ebayapp.mobilephone.MobilePhone;
 import io.kirill.ebayapp.mobilephone.MobilePhoneService;
-import java.time.Duration;
-import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class AppRunner implements CommandLineRunner {
+public class AppRunner {
   private static final int MINUTES_PERIOD = 10;
   private static final int EXPECTED_MARGIN_PERCENTAGE = 50;
 
   private final MobilePhoneService mobilePhoneService;
 
-  @Override
-  public void run(String... args) throws Exception {
-    Flux.fromStream(Stream.iterate(0, i -> i + 1))
-        .delayElements(Duration.ofMinutes(MINUTES_PERIOD))
-        .flatMap($ -> mobilePhoneService.getLatestPhonesFromEbay(MINUTES_PERIOD))
+  @Scheduled(fixedDelay = MINUTES_PERIOD * 60 * 1000)
+  public void run() {
+    mobilePhoneService.getLatestPhonesFromEbay(MINUTES_PERIOD)
         .filter(MobilePhone::hasAllDetails)
         .flatMap(mobilePhoneService::findResellPrice)
         .filter(phone -> phone.isProfitableToResell(EXPECTED_MARGIN_PERCENTAGE))
         .doOnNext(phone -> log.info("found good deal on \"{}\": asking price {}, cex price P{}", phone.fullName(), phone.getPrice(), phone.getResellPrice()))
+        .doOnError(error -> log.error("error during app run: {}", error.getMessage(), error))
         .subscribe();
   }
 }
--- src/main/java/io/kirill/ebayapp/EbayApplication.java
+++ src/main/java/io/kirill/ebayapp/EbayApplication.java
@@ -5,7 +5,9 @@ import io.kirill.ebayapp.common.configs.EbayConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableConfigurationProperties({EbayConfig.class, CexConfig.class})
 public class EbayApplication {
