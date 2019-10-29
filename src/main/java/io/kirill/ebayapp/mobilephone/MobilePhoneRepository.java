b30496c0dffbb8c9b208f43a542bb28518429bd5
kirill <immotional@aol.com>
2019-10-29 08:27:27 +0000

    cleanup

--- src/main/java/io/kirill/ebayapp/mobilephone/MobilePhoneRepository.java
+++ src/main/java/io/kirill/ebayapp/mobilephone/MobilePhoneRepository.java
@@ -2,5 +2,5 @@ package io.kirill.ebayapp.mobilephone;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface MobilePhoneRepository extends ReactiveMongoRepository<MobilePhone, String> {
+interface MobilePhoneRepository extends ReactiveMongoRepository<MobilePhone, String> {
 }
--- src/main/java/io/kirill/ebayapp/mobilephone/clients/telegram/TelegramClient.java
+++ src/main/java/io/kirill/ebayapp/mobilephone/clients/telegram/TelegramClient.java
@@ -1,26 +1,9 @@
 package io.kirill.ebayapp.mobilephone.clients.telegram;
 
-import static java.util.Collections.emptyList;
-import static java.util.Optional.ofNullable;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-
-import io.kirill.ebayapp.common.configs.CexConfig;
 import io.kirill.ebayapp.common.configs.TelegramConfig;
 import io.kirill.ebayapp.mobilephone.MobilePhone;
-import io.kirill.ebayapp.mobilephone.clients.cex.exceptions.CexSearchError;
-import io.kirill.ebayapp.mobilephone.clients.cex.models.SearchData;
-import io.kirill.ebayapp.mobilephone.clients.cex.models.SearchError;
-import io.kirill.ebayapp.mobilephone.clients.cex.models.SearchErrorResponse;
-import io.kirill.ebayapp.mobilephone.clients.cex.models.SearchErrorResponseWrapper;
-import io.kirill.ebayapp.mobilephone.clients.cex.models.SearchResponseWrapper;
-import io.kirill.ebayapp.mobilephone.clients.cex.models.SearchResult;
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.function.Function;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
