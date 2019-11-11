11e29e43645acba37e6a22422277cd77e6fc8f7e
kirill <immotional@aol.com>
2019-11-11 07:48:17 +0000

    more trigger words

--- src/main/java/io/kirill/ebayapp/mobilephone/clients/ebay/mappers/ItemMapper.java
+++ src/main/java/io/kirill/ebayapp/mobilephone/clients/ebay/mappers/ItemMapper.java
@@ -1,19 +1,18 @@
 package io.kirill.ebayapp.mobilephone.clients.ebay.mappers;
 
+import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.toMap;
+
 import io.kirill.ebayapp.mobilephone.MobilePhone;
 import io.kirill.ebayapp.mobilephone.clients.ebay.models.Price;
 import io.kirill.ebayapp.mobilephone.clients.ebay.models.item.Item;
 import io.kirill.ebayapp.mobilephone.clients.ebay.models.item.ItemImage;
 import io.kirill.ebayapp.mobilephone.clients.ebay.models.item.ItemProperty;
-import org.springframework.stereotype.Component;
-
 import java.time.Instant;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-
-import static java.util.Optional.ofNullable;
-import static java.util.stream.Collectors.toMap;
+import org.springframework.stereotype.Component;
 
 @Component
 public class ItemMapper {
@@ -22,7 +21,8 @@ public class ItemMapper {
 
   private static final String CONDITION_TRIGGER_WORDS = String.join("|",
       "has cracks", "no touchid", "no touch id", "no faceid", "home button fault", "has a crack", "has cracked",
-      "spares or parts", "screen cracked", "smashed screen", "touch id doesn't work", "cracked screen"
+      "spares or parts", "screen cracked", "smashed screen", "cracked screen", "is faulty",
+      "is cracked", "spares or repair", "doesn't work", "not working"
   );
   private static final String FAULTY_CONDITION = "Faulty";
 
