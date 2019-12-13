4805627f51c41d1cd780c51720c3cbd40ad74a7e
kirill <immotional@aol.com>
2019-12-13 14:58:21 +0000

    clear ebay ids from cache based on time

--- src/main/java/io/kirill/ebayapp/mobilephone/clients/ebay/EbayClient.java
+++ src/main/java/io/kirill/ebayapp/mobilephone/clients/ebay/EbayClient.java
@@ -1,11 +1,13 @@
 package io.kirill.ebayapp.mobilephone.clients.ebay;
 
+import static java.util.stream.Collectors.toList;
+
 import io.kirill.ebayapp.mobilephone.MobilePhone;
 import io.kirill.ebayapp.mobilephone.clients.ebay.mappers.ItemMapper;
 import io.kirill.ebayapp.mobilephone.clients.ebay.models.search.SearchResult;
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Predicate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -24,15 +26,15 @@ public class EbayClient {
   private final EbaySearchClient searchClient;
   private final ItemMapper itemMapper;
 
-  private Set<String> ids = new HashSet<>();
+  private Map<String, Instant> ids = new HashMap<>();
 
   public Flux<MobilePhone> getPhonesListedInTheLastMinutes(int minutes) {
     return authClient.accessToken()
         .flatMapMany(token -> searchClient.searchForAllInCategory(token, MOBILES_PHONES_CATEGORY_ID, Instant.now().minusSeconds(minutes * 60)))
         .filter(hasTrustedSeller)
-        .filter(searchResult -> !ids.contains(searchResult.getItemId()))
+        .filter(searchResult -> !ids.containsKey(searchResult.getItemId()))
         .flatMap(sr -> authClient.accessToken().flatMap(token -> searchClient.getItem(token, sr.getItemId())))
-        .doOnNext(item -> ids.add(item.getItemId()))
+        .doOnNext(item -> ids.put(item.getItemId(), Instant.now()))
         .map(itemMapper::toMobilePhone);
   }
 
@@ -43,8 +45,12 @@ public class EbayClient {
       searchResult.getSeller().getFeedbackScore() > MIN_FEEDBACK_SCORE;
 
 
-  @Scheduled(cron = "0 0 4 * * *")
+  @Scheduled(fixedRate = 900000)
   void clearIds() {
-    ids.clear();
+    var idsToRemove = ids.entrySet().stream()
+        .filter(e -> e.getValue().isBefore(Instant.now().minusSeconds(1800)))
+        .map(Map.Entry::getKey)
+        .collect(toList());
+    idsToRemove.forEach(ids::remove);
   }
 }
