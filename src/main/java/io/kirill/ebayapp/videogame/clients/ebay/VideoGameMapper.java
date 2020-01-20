e61b88e806bf4e5cc55d7edc223857df6d299486
kirill <immotional@aol.com>
2020-01-20 10:39:13 +0000

    remove emojis from title

--- src/main/java/io/kirill/ebayapp/videogame/clients/ebay/VideoGameMapper.java
+++ src/main/java/io/kirill/ebayapp/videogame/clients/ebay/VideoGameMapper.java
@@ -23,7 +23,7 @@ public class VideoGameMapper implements ItemMapper<VideoGame> {
   private static final String WORDS_TO_REMOVE_FROM_TITLE = String.join("|",
       "remastered", "playstation 4", "Nintendo switch", " - ", "sony", "ps4", "blu-ray", "Mirror", "New and sealed",
       "Brand new", "Sealed", "Game new", ",", "Microsoft", "Free post", "Used", "xbox one", "Uk pal", "Game code",
-      "Hits", "Tom clancys", "Great Condition"
+      "Hits", "Tom clancys", "Great Condition", "[^\\p{L}\\p{M}\\p{N}\\p{P}\\p{Z}\\p{Cf}\\p{Cs}\\s]"
   );
 
   private static final List<String> PLATFORMS = List.of("PS4", "PLAYSTATION 4", "NINTENDO SWITCH", "SWITCH", "XBOX ONE");
